f551a74b4bc275e77a933ecee7acf4eeee63f938
Taylor Jones <taylor@taylorwjones.com>
2021-09-27 16:40:18 -0500

    🚧  Added Axios as a dependency, started on fetchCards function.

---
+++ backend/ygo-cards/fetchCards.ts
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const fetchCards = () => {
+  let cardArray = [];
+
+  // fetch the YGOPRODECK API
+
+  // iterate through the returned data
+
+  // push data to array
+};
+
+module.exports = fetchCards;
