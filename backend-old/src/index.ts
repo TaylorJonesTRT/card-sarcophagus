432d05490aa1bb5998756e24e01e999255c512fc
Taylor Jones <taylor@taylorwjones.com>
2021-10-02 21:08:38 -0500

    :building_construction: Removed backend-old folder

--- backend-old/src/index.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable import/no-unresolved */
-/* eslint-disable import/extensions */
-import server from './app';
-
-server.listen(process.env.PORT || 4000, () => {
-  console.log(`Listening: http://localhost:${process.env.PORT}`);
-});
