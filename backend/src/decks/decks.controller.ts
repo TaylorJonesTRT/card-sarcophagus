01d67cc6be8a3a9935d21576a735d2f6174e4c54
Taylor Jones <taylor@taylorwjones.com>
2021-11-09 22:12:20 -0600

    :construction: RESTful endpoints for decks

--- backend/src/decks/decks.controller.ts
+++ backend/src/decks/decks.controller.ts
@@ -1,7 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
 import { DecksService } from './decks.service';
 
 @Controller('decks')
 export class DecksController {
   constructor(private readonly decksService: DecksService) {}
+
+  @Get()
+  showAllDecks() {
+    return 'Not implemented yet';
+  }
+
+  @Get(':id')
+  getDeck() {
+    return 'Not implemented yet';
+  }
+
+  @Post()
+  createNewDeck() {
+    return 'Not implemented yet';
+  }
+
+  @Put()
+  updateDeck() {
+    return 'Not implemented yet';
+  }
+
+  @Delete(':id')
+  removeDeck() {
+    return 'Not implemented yet';
+  }
 }
