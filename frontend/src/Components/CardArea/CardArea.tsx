4eef45fc3ae2a18d2c6a33c686ba43ec544e8962
Taylor Jones <taylor@taylorwjones.com>
2022-03-03 16:17:05 -0700

    :children_crossing: Basic styling completed for the cardArea using fake card images

--- frontend/src/Components/CardArea/CardArea.tsx
+++ frontend/src/Components/CardArea/CardArea.tsx
@@ -3,12 +3,21 @@
 import React from 'react';
 import BottomBar from '../BottomBar';
 import Header from '../Header';
+import cardBack from '../../Assets/card-back.png';
 
 const CardArea = () => {
+  // create fake card data here (use face down cards just to get stuff working)
+  const cardData = [cardBack, cardBack, cardBack, cardBack, cardBack, cardBack, cardBack, cardBack, cardBack, cardBack, cardBack, cardBack, cardBack, cardBack, cardBack, cardBack, cardBack, cardBack, cardBack, cardBack, cardBack, cardBack, cardBack, cardBack, cardBack];
   return (
-    <div className='App w-screen h-screen grid font-sans'>
+    <div className='App w-screen h-screen flex flex-col font-sans'>
       <Header />
-      <h2>Card Area</h2>
+      <div className='content bg-gray-200 flex-grow'>
+        <ul className='flex flex-row gap-4 flex-wrap m-3'>
+          {cardData.map((card: string | undefined) => {
+            return <li><img className='w-28 shadow' src={card} alt='ygo-card-back' /></li>;
+          })}
+        </ul>
+      </div>
       <BottomBar />
     </div>
   );
