b5b68e1511ce30db9c72b7272a80808ee1ddc057
Taylor Jones <taylor@taylorwjones.com>
2022-03-02 18:07:12 -0700

    :children_crossing: Styled the header and added a background gradient to the body

--- frontend/src/Components/Header/Header.tsx
+++ frontend/src/Components/Header/Header.tsx
@@ -4,12 +4,12 @@ import './Header.css';
 
 const Header = () => {
   return (
-    <header className='flex flex-row w-full h-9 justify-around shadow-xl'>
+    <header className='flex flex-row w-full h-9 justify-around shadow-xl bg-gradient-to-r from-gray-900 to-gray-700 text-white border-b-2 border-gray-700'>
       <h1 className='font-bold w-1/2 m-auto ml-6'>Card Sarcophagus</h1>
       <nav className='w-1/2 m-auto'>
         <ul className="flex flex-row justify-end">
-          <li className="mr-6"><Link to='/'>Cards</Link></li>
-          <li className="mr-6"><Link to='/decks'>Decks</Link></li>
+          <li className="mr-6 hover:underline"><Link to='/'>Cards</Link></li>
+          <li className="mr-6 hover:underline"><Link to='/decks'>Decks</Link></li>
         </ul>
       </nav>
     </header>
