44445bdccc88f103884d50598769eda53aa85705
Taylor Jones <taylor@taylorwjones.com>
2022-03-03 16:16:20 -0700

    :children_crossing: Made the bottom bar stick to the bottom of the viewport

--- frontend/src/Components/BottomBar/BottomBar.tsx
+++ frontend/src/Components/BottomBar/BottomBar.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const BottomBar =() => {
   return (
-    <footer className='bg-gradient-to-bl from-gray-800 to-gray-600'>
+    <footer className='h-40 fixed inset-x-0 bottom-0 bg-gradient-to-bl from-gray-800 to-gray-600'>
       Bottom Bar
     </footer>
   )
