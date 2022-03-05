5d840ec47b6e6d8814b4b17a196fb30bd52a79db
Taylor Jones <taylor@taylorwjones.com>
2022-03-04 18:45:01 -0700

    :children_crossing: Added MaterialUI search field and icon to the SortOptions side of the bottombar

--- frontend/src/Components/BottomBar/SortOptions/SortOptions.tsx
+++ frontend/src/Components/BottomBar/SortOptions/SortOptions.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import TextField from '@mui/material/TextField';
+import InputAdornment from '@mui/material/InputAdornment';
+import SearchIcon from '@mui/icons-material/Search';
 
 const SortOptions = () => {
   return (
-    <span className='m-3 w-1/2'>Testing!</span>
+    <div className='sort-options w-1/2 flex flex-col m-3'>
+      <TextField
+      id="input-with-icon-textfield"
+      label="Search"
+      InputProps={{
+        startAdornment: (
+          <InputAdornment position="start">
+            <SearchIcon />
+          </InputAdornment>
+        ),
+      }}
+      variant="standard"
+      />
+      <span>Testing!</span>
+    </div>
   )
 }
 
