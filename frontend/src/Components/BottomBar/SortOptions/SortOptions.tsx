6b570d9b4c1e125ebd86a90b68153021f0a60a06
Taylor Jones <taylor@taylorwjones.com>
2022-03-05 09:05:21 -0700

    :children_crossing: Added a search bar and radio buttons for sorting to the SortOptions component

--- frontend/src/Components/BottomBar/SortOptions/SortOptions.tsx
+++ frontend/src/Components/BottomBar/SortOptions/SortOptions.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import TextField from '@mui/material/TextField';
 import InputAdornment from '@mui/material/InputAdornment';
 import SearchIcon from '@mui/icons-material/Search';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
 
 const SortOptions = () => {
   return (
@@ -18,7 +23,21 @@ const SortOptions = () => {
       }}
       variant="standard"
       />
-      <span>Testing!</span>
+      <FormControl>
+        <FormLabel id='demo-radio-buttons-group-label'>Sort By:</FormLabel>
+        <RadioGroup
+          aria-labelledby='demo-radio-buttons-group-label'
+          defaultValue='name'
+          name='radio-buttons-group'
+          className='h-1'
+        >
+          <FormControlLabel value="card-name" control={<Radio />} label="Name" />
+          <FormControlLabel value="card-attack" control={<Radio />} label="Attack" />
+          <FormControlLabel value="card-defense" control={<Radio />} label="Defense" />
+          <FormControlLabel value="card-type" control={<Radio />} label="Card Type" />
+          <FormControlLabel value="card-level" control={<Radio />} label="Level/Rank" />
+        </RadioGroup>
+      </FormControl>
     </div>
   )
 }
