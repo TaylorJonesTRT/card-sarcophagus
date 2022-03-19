cbb5eb79ea6b3f44ca1dcaafa5ea4aee9f855f3c
Taylor Jones <taylor@taylorwjones.com>
2022-03-19 14:21:41 -0600

    :sparkles: Added an onClick and onKeyPress listener to the rendered cards. Logic to come later.

--- frontend/src/Components/CardArea/CardArea.tsx
+++ frontend/src/Components/CardArea/CardArea.tsx
@@ -6,6 +6,8 @@ import Header from '../Header';
 import cardBack from '../../Assets/card-back.png';
 
 const CardArea = () => {
+  const loadCardEditor = () => {};
+
   // create fake card data here (use face down cards just to get stuff working)
   const cardData = [
     cardBack,
@@ -41,7 +43,12 @@ const CardArea = () => {
         <ul className='flex flex-row gap-4 flex-wrap m-3 pt-10'>
           {cardData.map((card: string | undefined) => (
             <li>
-              <div role='button' tabIndex={0}>
+              <div
+                role='button'
+                tabIndex={0}
+                onClick={loadCardEditor}
+                onKeyPress={loadCardEditor}
+              >
                 <img className='w-28 shadow' src={card} alt='ygo-card-back' />
               </div>
             </li>
--- frontend/src/Components/DeckArea/DeckArea.tsx
+++ frontend/src/Components/DeckArea/DeckArea.tsx
@@ -1,12 +1,14 @@
 /* eslint-disable import/extensions */
 /* eslint-disable import/no-unresolved */
 import React from 'react';
+import BottomBar from '../BottomBar';
 import Header from '../Header';
 
 const DeckArea = () => (
-  <div className="App">
+  <div className='App'>
     <Header />
     <h2>Deck Area</h2>
+    <BottomBar />
   </div>
 );
 
