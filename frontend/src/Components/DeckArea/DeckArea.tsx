9966fbdf4dc7d2162835b25d353106df5fee5bb3
Taylor Jones <taylor@taylorwjones.com>
2022-03-22 12:32:19 -0600

    :children_crossing: Added a deckbox picture to the project and replaced the cardBacks with it in deckArea

--- frontend/src/Components/DeckArea/DeckArea.tsx
+++ frontend/src/Components/DeckArea/DeckArea.tsx
@@ -3,13 +3,30 @@
 import React from 'react';
 import BottomBar from '../BottomBar';
 import Header from '../Header';
+import deckBox from '../../Assets/deckbox.png';
 
-const DeckArea = () => (
-  <div className='App'>
-    <Header />
-    <h2>Deck Area</h2>
-    <BottomBar />
-  </div>
-);
+const DeckArea = () => {
+  const cardData = [deckBox, deckBox, deckBox];
+
+  return (
+    <div className='App'>
+      <Header />
+      <div className='content h-auto m-3 pt-10'>
+        <div className='decks'>
+          <ul className='flex flex-row gap-4'>
+            {cardData.map((card: string | undefined) => (
+              <li>
+                <div role='button' tabIndex={0}>
+                  <img className='w-28' src={card} alt='ygo-card-back' />
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <BottomBar />
+    </div>
+  );
+};
 
 export default DeckArea;
