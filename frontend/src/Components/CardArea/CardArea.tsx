da06eabf18c86fa39631b69a518a31fa7890fcc4
Taylor Jones <taylor@taylorwjones.com>
2022-03-22 13:42:21 -0600

    Fetching cards from backend with axios, displaying on frontend

--- frontend/src/Components/CardArea/CardArea.tsx
+++ frontend/src/Components/CardArea/CardArea.tsx
@@ -1,47 +1,36 @@
 /* eslint-disable import/extensions */
 /* eslint-disable import/no-unresolved */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import BottomBar from '../BottomBar';
 import Header from '../Header';
-import cardBack from '../../Assets/card-back.png';
 
 const CardArea = () => {
+  const [cards, setCards] = useState<any[]>();
   const loadCardEditor = () => {};
 
-  // create fake card data here (use face down cards just to get stuff working)
-  const cardData = [
-    cardBack,
-    cardBack,
-    cardBack,
-    cardBack,
-    cardBack,
-    cardBack,
-    cardBack,
-    cardBack,
-    cardBack,
-    cardBack,
-    cardBack,
-    cardBack,
-    cardBack,
-    cardBack,
-    cardBack,
-    cardBack,
-    cardBack,
-    cardBack,
-    cardBack,
-    cardBack,
-    cardBack,
-    cardBack,
-    cardBack,
-    cardBack,
-    cardBack,
-  ];
+  useEffect(() => {
+    const fetchCards = async () => {
+      const fetchedCards = await axios
+        .get('http://localhost:3001/api/cards')
+        .then((response) => {
+          setCards(response.data);
+        });
+      // setCards(fetchedCards);
+      // console.log(fetchedCards);
+      // console.log(cards);
+      console.log(cards);
+      return fetchedCards;
+    };
+    fetchCards();
+  }, []);
+
   return (
     <div className='App w-screen h-screen flex flex-col font-sans'>
       <Header />
       <div className='content h-auto'>
         <ul className='flex flex-row gap-4 flex-wrap m-3 pt-10'>
-          {cardData.map((card: string | undefined) => (
+          {cards!.map((card) => (
             <li>
               <div
                 role='button'
@@ -49,7 +38,11 @@ const CardArea = () => {
                 onClick={loadCardEditor}
                 onKeyPress={loadCardEditor}
               >
-                <img className='w-28 shadow' src={card} alt='ygo-card-back' />
+                <img
+                  className='w-28 shadow'
+                  src={card!.cardImage}
+                  alt='ygo-card-back'
+                />
               </div>
             </li>
           ))}
