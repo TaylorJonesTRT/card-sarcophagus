0fec4a02ad3a4fa14564ab0afb290f406d1e213d
Taylor Jones <taylor@taylorwjones.com>
2022-03-27 15:38:49 -0600

    Changed the props for cards and decks to be an array that houses it all

--- backend/src/users/schemas/users.schema.ts
+++ backend/src/users/schemas/users.schema.ts
@@ -1,5 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import { Document, Types, Schema as MongooseSchema } from 'mongoose';
 
 export type UsersDocument = Users & Document;
 
@@ -10,6 +10,12 @@ export class Users {
 
   @Prop({ required: true })
   password: string;
+
+  @Prop([{ type: MongooseSchema.Types.ObjectId, ref: 'Card' }])
+  ownedCards: [Types.ObjectId];
+
+  @Prop([{ type: MongooseSchema.Types.ObjectId, ref: 'Deck' }])
+  decks: [Types.ObjectId];
 }
 
 export const UsersSchema = SchemaFactory.createForClass(Users);
