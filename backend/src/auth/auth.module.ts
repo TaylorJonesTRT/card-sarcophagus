5693d1d28487e6b25af79817eb3678007ce2200f
Taylor Jones <taylor@taylorwjones.com>
2022-03-29 11:22:27 -0600

    Removed the mongoose pre save function as already in the Users module

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,10 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-this-alias */
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { Users, UsersSchema } from '../users/schemas/users.schema';
 import { MongooseModule } from '@nestjs/mongoose';
-import * as bcrypt from 'bcrypt';
 import { UsersModule } from '../users/users.module';
 import { LocalStrategy } from './local.strategy';
 import { PassportModule } from '@nestjs/passport';
@@ -13,21 +13,6 @@ import { PassportModule } from '@nestjs/passport';
   imports: [
     UsersModule,
     MongooseModule.forFeature([{ name: 'Users', schema: UsersSchema }]),
-    MongooseModule.forFeatureAsync([
-      {
-        name: 'Users',
-        useFactory: () => {
-          const schema = UsersSchema;
-          schema.pre<Users>('save', async function () {
-            const user = this;
-            const salt = await bcrypt.genSalt(10);
-            const hashedPassword = await bcrypt.hash(user.password, salt);
-            user.password = hashedPassword;
-          });
-          return schema;
-        },
-      },
-    ]),
     PassportModule,
   ],
   providers: [AuthService, LocalStrategy],
