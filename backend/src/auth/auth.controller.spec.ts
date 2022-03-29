d98c75319377381df59b81d9da5380ca11c888c7
Taylor Jones <taylor@taylorwjones.com>
2022-03-28 19:43:43 -0600

    Generated controllers and their test files for both users and auth

---
+++ backend/src/auth/auth.controller.spec.ts
@@ -0,0 +1,29 @@
+import { getModelToken } from '@nestjs/mongoose';
+import { Test, TestingModule } from '@nestjs/testing';
+import { Model } from 'mongoose';
+import { Users } from '../users/schemas/users.schema';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+
+describe('AuthController', () => {
+  let controller: AuthController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [AuthController],
+      providers: [
+        AuthService,
+        {
+          provide: getModelToken(Users.name),
+          useValue: Model,
+        },
+      ],
+    }).compile();
+
+    controller = module.get<AuthController>(AuthController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
