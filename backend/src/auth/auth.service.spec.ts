9cee877cd00132f3fef1488abe432fbb08877345
Taylor Jones <taylor@taylorwjones.com>
2022-03-28 19:53:43 -0600

    Added providers needed to pass tests

--- backend/src/auth/auth.service.spec.ts
+++ backend/src/auth/auth.service.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { Model } from 'mongoose';
+import { Users } from '../users/schemas/users.schema';
+import { getModelToken } from '@nestjs/mongoose';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: getModelToken(Users.name),
+          useValue: Model,
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
