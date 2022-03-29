df7e6d2da797386c26a705a989e050153a928b53
Taylor Jones <taylor@taylorwjones.com>
2022-03-28 19:54:47 -0600

    Added route to create a user and the logic behind it

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,7 +1,32 @@
+/* eslint-disable @typescript-eslint/no-this-alias */
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { Users, UsersSchema } from '../users/schemas/users.schema';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as bcrypt from 'bcrypt';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Users', schema: UsersSchema }]),
+    MongooseModule.forFeatureAsync([
+      {
+        name: 'Users',
+        useFactory: () => {
+          const schema = UsersSchema;
+          schema.pre<Users>('save', async function () {
+            const user = this;
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(user.password, salt);
+            user.password = hashedPassword;
+          });
+          return schema;
+        },
+      },
+    ]),
+  ],
   providers: [AuthService],
+  controllers: [AuthController],
+  exports: [AuthService],
 })
 export class AuthModule {}
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,32 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Injectable } from '@nestjs/common';
+import { Users, UsersDocument } from '../users/schemas/users.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectModel('Users') private readonly usersModel: Model<UsersDocument>,
+  ) {}
+
+  async createUser(email: string, password: string) {
+    const alreadyCreated = await this.usersModel.findOne({ email });
+
+    if (alreadyCreated) {
+      return { error: 'An account already exists with that email' };
+    }
+
+    const newUser = new this.usersModel({
+      email,
+      password,
+    });
+
+    newUser.save((err) => {
+      if (err) {
+        return console.log(err);
+      }
+    });
+    return { message: 'Account Created!' };
+  }
+}
