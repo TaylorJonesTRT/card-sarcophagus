c8e968f2ae8438e04bb6703e4c4c411db8ec4163
Taylor Jones <taylor@taylorwjones.com>
2022-04-09 17:23:49 -0600

    added login screen component

---
+++ frontend/src/Components/LoginScreen/LoginScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => (
+  <div className='left-bar'>
+    <span>hello</span>
+  </div>
+);
+
+export default Header;
