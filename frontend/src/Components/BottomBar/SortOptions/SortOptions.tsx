aba81197069fe9e21a92df2f217ad4401c0dabb0
Taylor Jones <taylor@taylorwjones.com>
2022-04-24 12:28:41 -0600

    Re arranged the sort options to be on top of the search bar

--- frontend/src/Components/BottomBar/SortOptions/SortOptions.tsx
+++ frontend/src/Components/BottomBar/SortOptions/SortOptions.tsx
@@ -3,20 +3,8 @@ import React from 'react';
 const SortOptions = () => (
   <div className='sort-options w-1/2 flex flex-col m-3'>
     <form>
-      <ul>
-        <li>
-          <label htmlFor='card-name' className='grid grid-rows-2'>
-            Search:
-            <input
-              type='text'
-              id='card-search'
-              name='card-name'
-              className='border-2 border-gray-400 rounded outline-none focus:border-blue-400'
-            />
-          </label>
-        </li>
-      </ul>
-      <ul className='flex flex-cols-5 gap-10 pt-5'>
+      <h2>Sort Cards By:</h2>
+      <ul className='flex flex-cols-5 gap-10 pt-1'>
         <li>
           <input
             type='radio'
@@ -68,6 +56,21 @@ const SortOptions = () => (
               className='mr-2'
             />
             Card Type
+          </label>
+        </li>
+      </ul>
+    </form>
+    <form className='pt-5'>
+      <ul>
+        <li>
+          <label htmlFor='card-name' className='grid grid-rows-2'>
+            Search:
+            <input
+              type='text'
+              id='card-search'
+              name='card-name'
+              className='border-2 border-gray-400 rounded outline-none focus:border-blue-400'
+            />
           </label>
         </li>
       </ul>
