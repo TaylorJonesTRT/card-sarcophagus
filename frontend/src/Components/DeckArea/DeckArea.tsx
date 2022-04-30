c22970f062f253a25667a662fd33514afdea8954
Taylor Jones <taylor@taylorwjones.com>
2022-04-30 12:14:59 -0600

    Refactored deckarea to now show all created decks by a user and implemented the deck modal

--- frontend/src/Components/DeckArea/DeckArea.tsx
+++ frontend/src/Components/DeckArea/DeckArea.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable consistent-return */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable import/extensions */
 /* eslint-disable import/no-unresolved */
@@ -6,10 +7,13 @@ import { useNavigate } from 'react-router-dom';
 import Cookies from 'universal-cookie';
 import axios from 'axios';
 import deckBox from '../../Assets/deckbox.png';
+import EditorModal from '../EditorModal';
 
 const DeckArea = (props: any) => {
-  const [decks, setDecks] = useState();
   const { checkJwt } = props;
+  const [decks, setDecks] = useState<any[]>([]);
+  const [clickedDeckName, setClickedDeckName] = useState<string>();
+  const [showEditor, setShowEditor] = useState<boolean>(false);
   const cardData = [deckBox, deckBox, deckBox];
   const navigate = useNavigate();
 
@@ -18,29 +22,54 @@ const DeckArea = (props: any) => {
     const fetchDecks = async () => {
       const cookies = new Cookies();
       const fetchedCards = await axios
-        .get('http://localhost:3001/api/cards')
+        .get('http://localhost:3001/api/decks', {
+          headers: {
+            Authorization: `Bearer ${cookies.get('carsar')}`,
+          },
+        })
         .then((response) => {
-          console.log(response.data);
+          setDecks(response.data.allDecks);
         })
         .catch((error) => {
-          if (error.response.status.statusCode === 401) {
+          if (error.response.status === 401) {
             cookies.remove('carsar');
-            navigate('/login');
+            navigate('/');
             return window.location.reload();
           }
-          return console.log(error.response);
         });
-      return fetchedCards;
     };
     fetchDecks();
   }, []);
 
+  const openNewDeckModal = () => {
+    checkJwt();
+    setClickedDeckName(undefined);
+    setShowEditor(true);
+  };
+
+  const openEditDeckModal = (deckName: string) => {
+    checkJwt();
+    setClickedDeckName(deckName);
+    setShowEditor(true);
+  };
+
+  const closeDeckModal = () => {
+    setShowEditor(false);
+  };
+
   return (
     <div className='w-full basis-full pt-3'>
       <div className='container w-[1240px] mx-auto decks'>
+        {showEditor && (
+          <EditorModal
+            clickedDeckName={clickedDeckName}
+            closeModal={closeDeckModal}
+            editorType='deck'
+          />
+        )}
         <ul className='flex flex-row gap-10'>
           <li key='addNewDeck' className='text-center'>
-            <div role='button' tabIndex={0}>
+            <div role='button' tabIndex={0} onClick={openNewDeckModal}>
               <img
                 className='w-28 opacity-50 grayscale'
                 src={deckBox}
@@ -49,15 +78,18 @@ const DeckArea = (props: any) => {
               <span>Add new deck</span>
             </div>
           </li>
-          {cardData.map((card: string | undefined) => (
+          {decks.map((deck: any) => (
             <li>
               <div
                 role='button'
                 tabIndex={0}
                 className='flex flex-col text-center'
+                onClick={(e) => {
+                  openEditDeckModal(deck.deckName);
+                }}
               >
-                <img className='w-28' src={card} alt='ygo-card-back' />
-                <span>Deck Name</span>
+                <img className='w-28' src={deckBox} alt='ygo-card-back' />
+                <span>{deck!.deckName}</span>
               </div>
             </li>
           ))}
